1b9910dddbf6dc9b00989914c493895bd249ccce
Vineet Dev <vindev@microsoft.com>
2019-03-01 18:05:46 +0530

    prettified

--- src/CustomInfiniteScrollView2/ISV.tsx
+++ src/CustomInfiniteScrollView2/ISV.tsx
@@ -18,32 +18,36 @@ export interface ISVState {
 }
 
 export default class ISV extends React.Component<{}, ISVState> {
-private observationPoint: ObservationPoint;
-    constructor(props: {}) {
-        super(props);
-        this.observationPoint = {
-            reference: ScrollContainerCoordinateRef.BOTTOM,
-            displacement: 100,
-            intersectionCallback: this.intersectionCallback
-          };
-          try {
-            const locationQueryString = window.location.search.substr(1);
-            const singleQueryStringArr = locationQueryString.split("&");
-            const reference = singleQueryStringArr[0].split("=")[1] === "top" ? ScrollContainerCoordinateRef.TOP : ScrollContainerCoordinateRef.BOTTOM;
-            const displacement = +singleQueryStringArr[1].split("=")[1];
-            this.observationPoint.reference = reference;
-            this.observationPoint.displacement = displacement;
-        }catch(e) {
-          console.error("You can pass reference and displacement in url query param as '?reference=top&displacement=-40'");
-        }
-          this.state = {
-            data: initialData,
-            anchorElement: {
-              elemSelector: `div[data-val="${initialData[3]}"]`
-            }
-          };
+  private observationPoint: ObservationPoint;
+  constructor(props: {}) {
+    super(props);
+    this.observationPoint = {
+      reference: ScrollContainerCoordinateRef.BOTTOM,
+      displacement: 100,
+      intersectionCallback: this.intersectionCallback
+    };
+    try {
+      const locationQueryString = window.location.search.substr(1);
+      const singleQueryStringArr = locationQueryString.split("&");
+      const reference =
+        singleQueryStringArr[0].split("=")[1] === "top"
+          ? ScrollContainerCoordinateRef.TOP
+          : ScrollContainerCoordinateRef.BOTTOM;
+      const displacement = +singleQueryStringArr[1].split("=")[1];
+      this.observationPoint.reference = reference;
+      this.observationPoint.displacement = displacement;
+    } catch (e) {
+      console.error(
+        "You can pass reference and displacement in url query param as '?reference=top&displacement=-40'"
+      );
     }
-  
+    this.state = {
+      data: initialData,
+      anchorElement: {
+        elemSelector: `div[data-val="${initialData[3]}"]`
+      }
+    };
+  }
 
   private intersectionCallback: ScrollIntersectionCallback = (
     elem: HTMLElement,
@@ -58,15 +62,15 @@ private observationPoint: ObservationPoint;
   };
 
   render() {
-      let intersectionPointStyle = {};
-      if(this.observationPoint.reference === ScrollContainerCoordinateRef.TOP) {
-        intersectionPointStyle = {top: this.observationPoint.displacement * -1};
-      } else {
-        intersectionPointStyle = {bottom: this.observationPoint.displacement};
-      }
+    let intersectionPointStyle = {};
+    if (this.observationPoint.reference === ScrollContainerCoordinateRef.TOP) {
+      intersectionPointStyle = { top: this.observationPoint.displacement * -1 };
+    } else {
+      intersectionPointStyle = { bottom: this.observationPoint.displacement };
+    }
     return (
       <div className="viewContainer">
-      <div className="intersectionPoint" style={intersectionPointStyle}></div>
+        <div className="intersectionPoint" style={intersectionPointStyle} />
         {this.state.data.length > 0 ? (
           <InfiniteScrollBase
             observationPoints={[this.observationPoint]}
@@ -100,8 +104,8 @@ private observationPoint: ObservationPoint;
     observationPoint: ObservationPoint
   ) => {
     if (observationPoint === this.observationPoint) {
-        console.log("=====Intersecting downwards=====");
-        console.log(elem);
+      console.log("=====Intersecting downwards=====");
+      console.log(elem);
     }
   };
 }
